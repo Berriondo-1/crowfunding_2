961660788e51d3c46a00391c07b2dc267c190a54
Nicolas rodriguez quintero <rodriguezquinteronicolas06@gmail.com>
2025-11-03 19:23:57 -0500

    contenido de vistas prueba

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,2 @@
+<h1>Panel de Administración</h1>
+<p>Bienvenido {{ Auth::user()->nombre_completo ?? Auth::user()->name }}</p>
---
+++ resources/views/colaborador/dashboard.blade.php
@@ -0,0 +1,2 @@
+<h1>Panel de Colaborador</h1>
+<p>Bienvenido {{ Auth::user()->nombre_completo ?? Auth::user()->name }}</p>
