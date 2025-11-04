c1ea362fc5a8f6adede931431c6661378c550fcc
Nicolas rodriguez quintero <rodriguezquinteronicolas06@gmail.com>
2025-11-03 19:20:22 -0500

    creacion de rutas para los roles

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,38 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\ProfileController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Panel de ADMIN
+Route::get('/admin', AdminDashboard::class)
+    ->middleware(['auth', 'role:ADMIN'])
+    ->name('admin.dashboard');
+
+// Panel de AUDITOR
+Route::get('/auditor', function () {
+    return view('auditor.dashboard');
+})->middleware(['auth', 'role:AUDITOR'])->name('auditor.dashboard');
+
+// Panel de CREADOR
+Route::get('/creator', function () {
+    return view('creator.dashboard');
+})->middleware(['auth', 'role:CREADOR'])->name('creator.dashboard');
+
+// Panel de COLABORADOR
+Route::get('/colaborador', function () {
+    return view('colaborador.dashboard');
+})->middleware(['auth', 'role:COLABORADOR'])->name('colaborador.dashboard');
+
+// Dashboard general (fallback)
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
